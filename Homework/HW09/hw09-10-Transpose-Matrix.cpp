337aebc4285b947536d200f7d9fe59e65b50e646
Jpwrbd <jpwrbd456@gmail.com>
2023-09-19 15:57:07 +0700

    up

--- Homework/HW09/hw09-10-Transpose-Matrix.cpp
+++ Homework/HW09/hw09-10-Transpose-Matrix.cpp
@@ -1,8 +1,8 @@
 /*
     ผู้ใช้กรอกจำนวนที่ต้องการในอาเรย์ 2 มิติ พร้อมกับกรอกค่าลงไปในอาเรย์จนครบทุกค่า จากนั้นให้คุณทำการ Transpose ค่าในอาเรย์และแสดงผลบนหน้าจอ
-    
+
     Test case:
-        Array element : 
+        Array element :
             2 3
         Input :
             9 8 7
@@ -17,7 +17,7 @@
             7 5
 
     Test case:
-        Array element : 
+        Array element :
             3 6
         Input :
             1 2 3 4 5 6
@@ -36,3 +36,68 @@
             5 11 17
             6 12 18
 */
+
+#include <stdio.h>
+
+int main()
+{
+
+    int row, column, temp;
+
+    // สร้างarray พร้อมรับข้อมูล
+    printf("Array element : \n");
+    scanf("%d %d", &column, &row);
+    int array[column][row];
+    for (int i = 0; i < column; i++)
+    {
+        for (int k = 0; k < row; k++)
+        {
+            scanf("%d", &array[i][k]);
+        } // end for
+    }     // end for
+
+    // แสดงค่า array
+    printf("Array\n");
+    for (int i = 0; i < column; i++)
+    {
+        for (int k = 0; k < row; k++)
+        {
+            printf("%d", array[i][k]);
+            if (row > k + 1)
+            {
+                printf(" ");
+            } // end if
+        }     // end for
+        printf("\n");
+    } // end for
+
+    // หมุน array โดยสร้างอีกตัว
+    temp = column;
+    column = row;
+    row = temp;
+    int arraytranspose[column][row];
+    for (int i = 0; i < column; i++)
+    {
+        for (int k = 0; k < row; k++)
+        {
+            arraytranspose[i][k] = array[k][i];
+        } // end for
+    }     // end for
+
+    // แสดงค่า
+    printf("Array Transpose\n");
+    for (int i = 0; i < column; i++)
+    {
+        for (int k = 0; k < row; k++)
+        {
+            printf("%d", arraytranspose[i][k]);
+            if (row > k + 1)
+            {
+                printf(" ");
+            } // end if
+        }     // end for
+        printf("\n");
+    } // end for
+
+    return 0;
+} // end main
